e26b9c2cc88a8e2382a9d8c852a348431811d535
Nkwi Cyril <akinimbomnkwi@gmail.com>
2026-07-06 18:50:11 +0100

    feat: add Vercel Analytics integration to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -35,7 +36,7 @@ export default function RootLayout({
       lang="en"
       className={`${figtree.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}<Analytics /></body>
     </html>
   );
 }
